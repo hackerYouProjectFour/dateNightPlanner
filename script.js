075a9fb17ae7178c69fcbe5949dcf808057c6e16
Theodore Belo <theo_b@me.com>
2019-08-28 17:55:02 -0400

    Added movieApp

--- script.js
+++ script.js
@@ -8,9 +8,7 @@ app.queryParams.cityCode = 89;
 app.queryParams.entity = 'city';
 app.queryParams.count = 20;
 app.queryParams.offset = 0;
-
 app.restaurants = [];
-
 // Collect user input
 app.getRestorantInfo = function(){
     // Creat object to hold user picked info
@@ -28,7 +26,6 @@ app.getRestorantInfo = function(){
         }
     });
 }
-
 // Make AJAX request with user inputted to the zomato
 app.getInfo = function() {
     // make api call
@@ -55,17 +52,64 @@ app.getInfo = function() {
         
     })
 }
-
 // Display data on the page
 app.displayInfo = function() {
 
 }
-
 // Start app
 app.init = function() {
     app.getRestorantInfo();
 }
 
+const movieApp = {};
+
+movieApp.showtimeUrl = `http://data.tmsapi.com/v1.1/movies/showings`;
+movieApp.apiKey = `asa363es8bybmdvt64csjra7`;
+movieApp.zip = `M6K 3R4`;
+movieApp.radius = 3;
+movieApp.units = `km`;
+movieApp.theatreList = {};
+movieApp.data = [];
+movieApp.theatres = [];
+movieApp.pullData = function () {
+    return $.ajax({
+        url: movieApp.showtimeUrl,
+        method: 'GET',
+        datatype: 'json',
+        data: {
+            api_key: movieApp.apiKey,
+            startDate: movieApp.getTodaysDate,
+            //TO BE CHANGED
+            zip: movieApp.zip,
+            radius: movieApp.radius,
+            units: movieApp.units
+        },
+    });
+};
+movieApp.getTodaysDate = function () {
+    const today = new Date();
+    const date = today.getFullYear() + `-` + ('0' + (today.getMonth() + 1)).slice(-2) + `-` + ('0' + today.getDate()).slice(-2);
+    return date;
+}
+movieApp.storeData = function () {
+    movieApp.pullData().then(function (results) {
+        const duplicateTheatres = [];
+        results.forEach(function(result){
+            duplicateTheatres.push(result.showtimes[0].theatre.name);
+        })
+        const uniqueSet = new Set(duplicateTheatres);
+        movieApp.theatreList = [...uniqueSet];
+        console.log(movieApp.theatreList);
+    });
+
+};
+
+movieApp.init = function () {
+    movieApp.getTodaysDate();
+    movieApp.storeData();
+}
+
 $(function() {
     app.init();
+    movieApp.init();  
 });
